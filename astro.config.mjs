7c4b402f5c6f2f9012fec852d553393eddb85a98
Ying Liu <yingliu4203@gmail.com>
2026-08-20 17:51:02 -0700

    fix site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://misnotes.uk',
+    base: '/',
     integrations: [
         starlight({
             title: 'MIS Notes',
