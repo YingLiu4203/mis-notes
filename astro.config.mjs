017eaeeea373c529f6d9467e90eb2e921c239ce5
Ying Liu <yingliu4203@gmail.com>
2026-08-20 18:29:13 -0700

    add astro cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,31 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://misnotes.uk',
-    base: '/',
-    integrations: [
-        starlight({
-            title: 'MIS Notes',
-            sidebar: [
-                'ch01_mis/mis',
-                'ch02_information/information',
-                'ch03_hardware/hardware',
-                'ch04_software/software',
-                'ch05_network/network',
-                'ch06_database/database',
-                'ch07_web/web',
-                'ch08_cryptograph/cryptograph',
-                'ch09_security/security',
-                'ch10_bitcoin/bitcoin',
-                'ch11_netflix/netflix',
-            ],
-        }),
-    ],
+  site: 'https://misnotes.uk',
+  base: '/',
+
+  integrations: [
+      starlight({
+          title: 'MIS Notes',
+          sidebar: [
+              'ch01_mis/mis',
+              'ch02_information/information',
+              'ch03_hardware/hardware',
+              'ch04_software/software',
+              'ch05_network/network',
+              'ch06_database/database',
+              'ch07_web/web',
+              'ch08_cryptograph/cryptograph',
+              'ch09_security/security',
+              'ch10_bitcoin/bitcoin',
+              'ch11_netflix/netflix',
+          ],
+      }),
+  ],
+
+  adapter: cloudflare(),
 });
